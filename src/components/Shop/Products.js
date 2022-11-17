6617ba9f111ba7a08c9d057594a90cc31ba62b2d
Rob Horner <rdhjr81@gmail.com>
2022-11-17 11:08:33 -0500

    add more items & display in lsit

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,23 @@
+
+import { useSelector } from 'react-redux';
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const items = useSelector(state => state.items);
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {
+          items.map(item => 
+            <ProductItem 
+              key={item.id} 
+              title={item.title}
+              price={item.price}
+              description={item.description}
+              />)
+        }
       </ul>
     </section>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,24 @@
 import { createSlice, createStore } from "@reduxjs/toolkit";
 
-const initialCartState ={
-    displayCart: false,
-    items:[]
-}
+const initialCartState = {
+  displayCart: false,
+  cart:[],
+  items: [
+    { id: 1, title: "Test Item", price: 6 ,description:'This is a first product - amazing!'},
+    { id: 2, title: "Leather Boots", price: 12,description:'Now 10% less smelly!'},
+    { id: 3, title: "Rain Jacket", price: 10,description:'Who\'s soaked? Not you!'},
+    { id: 3, title: "Wool Slippers", price: 20, description:'Unparalleled warmth'},
+],
+};
 
 const cartSlice = createSlice({
-    initialState: initialCartState,
-    name: 'cart',
-    reducers:{
-        toggleDisplayCart(state){
-            state.displayCart = !state.displayCart
-        }
-    }
+  initialState: initialCartState,
+  name: "cart",
+  reducers: {
+    toggleDisplayCart(state) {
+      state.displayCart = !state.displayCart;
+    },
+  },
 });
 
 const store = createStore(cartSlice.reducer);
