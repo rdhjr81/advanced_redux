f328f0de9b11319b4a5c13075beada2355126640
Rob Horner <rdhjr81@gmail.com>
2022-11-17 11:50:37 -0500

    fix number of items in cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,9 +1,20 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {cartActions} from '../../store'
 import classes from './CartButton.module.css';
 
+const getNumberOfItemsInCart = (cartItems) => {
+  let total = 0;
+  cartItems.forEach(cartItem => {
+    total+= cartItem.quantity;
+  });
+  return total;
+}
+
 const CartButton = (props) => {
   const dispatch = useDispatch();
+
+  const cartItems = useSelector(state => state.cartItems);
+  const totalNumberOfItems = getNumberOfItemsInCart(cartItems);
 
   const clickHandler = () => {
     dispatch(cartActions.toggleDisplayCart())
@@ -11,7 +22,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={clickHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalNumberOfItems}</span>
     </button>
   );
 };
