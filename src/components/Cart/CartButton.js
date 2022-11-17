a8a6485fb3c7d1080ed3d7564b86843718035830
Rob Horner <rdhjr81@gmail.com>
2022-11-17 10:54:50 -0500

    use action to toggle displaying cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
+import { useDispatch } from 'react-redux';
+import {cartActions} from '../../store'
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const clickHandler = () => {
+    dispatch(cartActions.toggleDisplayCart())
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={clickHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
