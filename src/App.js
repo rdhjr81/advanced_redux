a6eb55604674d1d6d32a707d5f397aa9111f1e5a
Rob Horner <rdhjr81@gmail.com>
2022-11-18 10:14:21 -0500

    persist cart

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,10 @@ import Products from "./components/Shop/Products";
 
 function App() {
     const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+    const cartItems = useSelector(state => state.cart.cartItems);
+    useEffect(()=>{
+      fetch('https://react-http-e781c-default-rtdb.firebaseio.com/cart.json', {method:'PUT', body: JSON.stringify(cartItems)});
+    },[cartItems]);
   return (
       <Layout>
         {cartIsVisible && <Cart />}
